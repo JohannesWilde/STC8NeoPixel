ba2c62de7e35f79e4a21203d385f4a9ae07af323
Johannes Wilde <git@jowil.de>
2026-04-03 23:03:49 +0200

    WIP: let the compiler draw the draft for what I need - all I want to do, is find out the correct timing where necessary.

--- src/main.c
+++ src/main.c
@@ -30,10 +30,127 @@
 
 static uint8_t preScalerOne = PRE_SCALER_ONE_INIT;
 
-static uint32_t neoPixelData[3];
 
-void show(uint32_t const * data, uint8_t const length) __naked
+#define NEO_PIXEL_DATA_BYTES_PER_PIXEL 4
+#define NEO_PIXEL_DATA_OFFSET_RED 1
+#define NEO_PIXEL_DATA_OFFSET_GREEN 0
+#define NEO_PIXEL_DATA_OFFSET_BLUE 2
+#define NEO_PIXEL_DATA_OFFSET_WHITE 3
+
+static uint8_t neoPixelData[4 * /*bytes per pixel*/4];
+
+
+
+void show(uint8_t const * data, uint8_t const length) /*__naked*/
 {
+
+    // for (uint8_t index = 0; length > index; ++index)
+    // {
+    //     uint8_t datum = data[index];
+    //     for (uint8_t bitIndex = 8; 0 < bitIndex; --index)
+    //     {
+    //         // MSb sent first.
+    //         bool const bitValue = datum & 0x80;
+
+    //         NEO_PIXEL_PIN = 1;
+    //         if (bitValue)
+    //         {
+    //             NOP(); // 19.2
+    //         }
+    //         else
+    //         {
+    //             NOP(); // 9.6
+    //         }
+    //         NEO_PIXEL_PIN = 0;
+    //         if (bitValue)
+    //         {
+    //             NOP(); // 10.8
+    //         }
+    //         else
+    //         {
+    //             NOP(); // 20.4
+    //         }
+
+    //         datum <<= 1;
+    //     }
+    // }
+
+
+    // _show:
+    //     ar7 = 0x07
+    //     ar6 = 0x06
+    //     ar5 = 0x05
+    //     ar4 = 0x04
+    //     ar3 = 0x03
+    //     ar2 = 0x02
+    //     ar1 = 0x01
+    //     ar0 = 0x00
+    //     push	_bp
+    //     mov	_bp,sp
+    //     mov	r5, dpl
+    //     mov	r6, dph
+    //     mov	r7, b
+    // ;	C:\Users\User\Documents\STC\STC8NeoPixel\src\main.c:47: for (uint8_t index = 0; length > index; ++index)
+    //     mov	a,_bp
+    //     add	a,#0xfd
+    //     mov	r0,a
+    //     clr	c
+    //     mov	a,#0x00
+    //     subb	a,@r0
+    //     jc	00143$
+    //     ljmp	00108$
+    // 00143$:
+    // ;	C:\Users\User\Documents\STC\STC8NeoPixel\src\main.c:49: uint8_t datum = data[index];
+    //     mov	dpl,r5
+    //     mov	dph,r6
+    //     mov	b,r7
+    //     lcall	__gptrget
+    //     mov	r7,a
+    // ;	C:\Users\User\Documents\STC\STC8NeoPixel\src\main.c:50: for (uint8_t bitIndex = 8; 0 < bitIndex; --index)
+    // 00110$:
+    // ;	C:\Users\User\Documents\STC\STC8NeoPixel\src\main.c:53: bool const bitValue = datum & 0x80;
+    //     mov	a,r7
+    //     rlc	a
+    //     mov	b0,c
+    // ;	C:\Users\User\Documents\STC\STC8NeoPixel\src\main.c:55: NEO_PIXEL_PIN = 1;
+    // ;	assignBit
+    //     setb	_P5_5
+    // ;	C:\Users\User\Documents\STC\STC8NeoPixel\src\main.c:56: if (bitValue)
+    //     jb	b0,00144$
+    //     ljmp	00102$
+    // 00144$:
+    // ;	C:\Users\User\Documents\STC\STC8NeoPixel\src\main.c:58: NOP(); // 19.2
+    //     NOP
+    //     ljmp	00103$
+    // 00102$:
+    // ;	C:\Users\User\Documents\STC\STC8NeoPixel\src\main.c:62: NOP(); // 9.6
+    //     NOP
+    // 00103$:
+    // ;	C:\Users\User\Documents\STC\STC8NeoPixel\src\main.c:64: NEO_PIXEL_PIN = 0;
+    // ;	assignBit
+    //     clr	_P5_5
+    // ;	C:\Users\User\Documents\STC\STC8NeoPixel\src\main.c:65: if (bitValue)
+    //     jb	b0,00145$
+    //     ljmp	00105$
+    // 00145$:
+    // ;	C:\Users\User\Documents\STC\STC8NeoPixel\src\main.c:67: NOP(); // 10.8
+    //     NOP
+    //     ljmp	00106$
+    // 00105$:
+    // ;	C:\Users\User\Documents\STC\STC8NeoPixel\src\main.c:71: NOP(); // 20.4
+    //     NOP
+    // 00106$:
+    // ;	C:\Users\User\Documents\STC\STC8NeoPixel\src\main.c:74: datum <<= 1;
+    //     mov	a,r7
+    //     add	a,acc
+    //     mov	r7,a
+    // ;	C:\Users\User\Documents\STC\STC8NeoPixel\src\main.c:50: for (uint8_t bitIndex = 8; 0 < bitIndex; --index)
+    //     ljmp	00110$
+    // ;	C:\Users\User\Documents\STC\STC8NeoPixel\src\main.c:47: for (uint8_t index = 0; length > index; ++index)
+    // 00108$:
+    // ;	C:\Users\User\Documents\STC\STC8NeoPixel\src\main.c:123: }
+    //     pop	_bp
+    //     ret
 
 
     // WS2812B
@@ -118,10 +235,23 @@ void main()
 
     interrupts(); // enable interrupts
 
-    neoPixelData[0] = (0xffull << 24) | (0x00ull << 16) | (0x00ull << 8) | (0x00ull << 0);
-    neoPixelData[1] = (0x00ull << 24) | (0xffull << 16) | (0x00ull << 8) | (0x00ull << 0);
-    neoPixelData[2] = (0x00ull << 24) | (0x00ull << 16) | (0xffull << 8) | (0x00ull << 0);
-    show(neoPixelData, 1);
+    neoPixelData[0 * NEO_PIXEL_DATA_BYTES_PER_PIXEL + NEO_PIXEL_DATA_OFFSET_RED] = 0xff;
+    neoPixelData[0 * NEO_PIXEL_DATA_BYTES_PER_PIXEL + NEO_PIXEL_DATA_OFFSET_GREEN] = 0x00;
+    neoPixelData[0 * NEO_PIXEL_DATA_BYTES_PER_PIXEL + NEO_PIXEL_DATA_OFFSET_BLUE] = 0x00;
+    neoPixelData[0 * NEO_PIXEL_DATA_BYTES_PER_PIXEL + NEO_PIXEL_DATA_OFFSET_WHITE] = 0x00;
+    neoPixelData[1 * NEO_PIXEL_DATA_BYTES_PER_PIXEL + NEO_PIXEL_DATA_OFFSET_RED] = 0x00;
+    neoPixelData[1 * NEO_PIXEL_DATA_BYTES_PER_PIXEL + NEO_PIXEL_DATA_OFFSET_GREEN] = 0xff;
+    neoPixelData[1 * NEO_PIXEL_DATA_BYTES_PER_PIXEL + NEO_PIXEL_DATA_OFFSET_BLUE] = 0x00;
+    neoPixelData[1 * NEO_PIXEL_DATA_BYTES_PER_PIXEL + NEO_PIXEL_DATA_OFFSET_WHITE] = 0x00;
+    neoPixelData[2 * NEO_PIXEL_DATA_BYTES_PER_PIXEL + NEO_PIXEL_DATA_OFFSET_RED] = 0x00;
+    neoPixelData[2 * NEO_PIXEL_DATA_BYTES_PER_PIXEL + NEO_PIXEL_DATA_OFFSET_GREEN] = 0x00;
+    neoPixelData[2 * NEO_PIXEL_DATA_BYTES_PER_PIXEL + NEO_PIXEL_DATA_OFFSET_BLUE] = 0xff;
+    neoPixelData[2 * NEO_PIXEL_DATA_BYTES_PER_PIXEL + NEO_PIXEL_DATA_OFFSET_WHITE] = 0x00;
+    neoPixelData[3 * NEO_PIXEL_DATA_BYTES_PER_PIXEL + NEO_PIXEL_DATA_OFFSET_RED] = 0x00;
+    neoPixelData[3 * NEO_PIXEL_DATA_BYTES_PER_PIXEL + NEO_PIXEL_DATA_OFFSET_GREEN] = 0x00;
+    neoPixelData[3 * NEO_PIXEL_DATA_BYTES_PER_PIXEL + NEO_PIXEL_DATA_OFFSET_BLUE] = 0x00;
+    neoPixelData[3 * NEO_PIXEL_DATA_BYTES_PER_PIXEL + NEO_PIXEL_DATA_OFFSET_WHITE] = 0xff;
+    show(neoPixelData, /*bytes*/ 4);
 
     while (true)
     {
