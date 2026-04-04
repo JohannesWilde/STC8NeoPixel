d7d9e4f571ac44ee9fc1bed7b0875ae9f95cc369
Johannes Wilde <git@jowil.de>
2026-04-04 17:41:59 +0200

    Support slower frequencies - but 4 MHz just barely.

--- src/main.c
+++ src/main.c
@@ -21,7 +21,7 @@
 
 #define F_WAKEUP_TIMER 36075  // Hz
 #define F_IRC 24000000ull  // Hz
-#define CLOCK_DIVISOR 1
+#define CLOCK_DIVISOR 6
 #define F_CPU (F_IRC / CLOCK_DIVISOR)  // Hz
 #define F_SYS_TICK 20  // Hz
 
@@ -54,13 +54,8 @@ void show(uint8_t const * data, uint8_t const length) __reentrant __naked
     UNUSED(length);
 
     // WS2812B
-    // 0:  high 0.40 us, low 0.85 us
     // 1:  high 0.80 us, low 0.45 us
-    //
-    // @24 MHz:
-    // 0:  high  9.6 clk, low 20.4 clk
-    // 1:  high 19.2 clk, low 10.8 clk
-    COMPILE_TIME_ASSERT(24000000 == F_CPU);
+    // 0:  high 0.40 us, low 0.85 us
 
     // b - memory type
     // [dpl, dph] - *data
@@ -99,14 +94,20 @@ void show(uint8_t const * data, uint8_t const length) __reentrant __naked
     "	mov	r7,a\n"         // datm << 1                        1 [1]
 
     // The following is the inner loop and must be timed precisely for each bit to be correct for the WS2812.
-    // Please note, that as long as the inter-bit times are smaller than the reset time, the will be cascaded correctly.
-    // So not timing requirements in between byte - apart from being less than 50 us apart.
-                            //                                                              (1)     (0)
+    // Please note, that as long as the inter-bit times are smaller than the reset time, it will be cascaded correctly.
+    // So not timing requirements in between bits - apart from being less than 50 us apart.
+
+
+#if (24000000 == F_CPU)
+    // @24 MHz:
+    // 1:  high 19.2 clk, low 10.8 clk
+    // 0:  high  9.6 clk, low 20.4 clk
+
     "	setb	_P5_5\n"    //                                  2 [1]                       1       1
     "	jc	007$\n"         //                                  2 [1/3]                     4       2
     "	ljmp	008$\n"     //                                  3 [3]                               5
 
-    // bit 1 transmission
+    // bit "1" transmission
     "007$:\n"
     "   mov r5,#5\n"        // 5 = 4 [jump] + 1 [not jump]      2 [1]                       5
     "013$:\n"
@@ -118,7 +119,7 @@ void show(uint8_t const * data, uint8_t const length) __reentrant __naked
     // "	nop	\n"             //                                                          _8
     "	ljmp	009$\n"     //                                  3 [3]                       _11
 
-    // bit 0 transmission
+    // bit "0" transmission
     "008$:\n"
     "	nop	\n"             //                                                                      6
     "	nop	\n"             //                                                                      7
@@ -131,6 +132,94 @@ void show(uint8_t const * data, uint8_t const length) __reentrant __naked
     "   djnz r5,015$\n"     // Decrement register and jump if not Zero     2 [2/3]                  _19
     "	NOP	\n"             //                                                                      _20
     "009$:\n"
+#elif (16000000 == F_CPU)
+    // @16 MHz
+    // 1:  high 12.8 clk, low  7.2 clk
+    // 0:  high  6.4 clk, low 13.6 clk
+
+    "	setb	_P5_5\n"    //                                  2 [1]                       1       1
+    "	jc	007$\n"         //                                  2 [1/3]                     4       2
+    "	ljmp	008$\n"     //                                  3 [3]                               5
+
+    // bit "1" transmission
+    "007$:\n"
+    "   mov r5,#3\n"        // 3 = 2 [jump] + 1 [not jump]      2 [1]                       5
+    "013$:\n"
+    "   djnz r5,013$\n"     // Decrement register and jump if not Zero     2 [2/3]          13
+    "	clr	_P5_5\n"        //                                  2 [1]                       _1
+    // "	nop	\n"             //                                                          _2
+    // "	nop	\n"             //                                                          _3
+    // "	nop	\n"             //                                                          _4
+    "	ljmp	009$\n"     //                                  3 [3]                       _7
+
+    // bit "0" transmission
+    "008$:\n"
+    "	nop	\n"             //                                                                      6
+    "	clr	_P5_5\n"        //                                  2 [1]                               _1
+    "   mov r5,#1\n"        // 4 - 3 = 3 - 3 [jump] + 1 [not jump]      2 [1]                       _2
+    "015$:\n"
+    "   djnz r5,015$\n"     // Decrement register and jump if not Zero     2 [2/3]                  _13
+    "	NOP	\n"             //                                                                      _14
+    "009$:\n"
+#elif (8000000 == F_CPU)
+    // @8 MHz
+    // 1:  high  6.4 clk, low  3.6 clk
+    // 0:  high  3.2 clk, low  6.8 clk
+
+    "	jc	007$\n"         //                                  2 [1/3]
+    "	ljmp	008$\n"     //                                  3 [3]
+
+    // bit "1" transmission
+    "007$:\n"
+    "	setb	_P5_5\n"    //                                  2 [1]                       1
+    "	nop	\n"             //                                                              2
+    "	nop	\n"             //                                                              3
+    "	nop	\n"             //                                                              4
+    "	nop	\n"             //                                                              5
+    "	nop	\n"             //                                                              6
+    "	clr	_P5_5\n"        //                                  2 [1]                       _1
+    "	ljmp	009$\n"     //                                  3 [3]                       _4
+
+    // bit "0" transmission
+    "008$:\n"
+    "	setb	_P5_5\n"    //                                  2 [1]                               1
+    "	nop	\n"             //                                                                      2
+    "	nop	\n"             //                                                                      3
+    "	clr	_P5_5\n"        //                                  2 [1]                               _1
+    "	nop	\n"             //                                                                      _2
+    "	nop	\n"             //                                                                      _3
+    "	nop	\n"             //                                                                      _4
+    "	nop	\n"             //                                                                      _5
+    "	nop	\n"             //                                                                      _6
+    "	nop	\n"             //                                                                      _7
+    "009$:\n"
+#elif (4000000 == F_CPU)
+    // @4 MHz
+    // 1:  high  3.2 clk, low  1.8 clk
+    // 0:  high  1.6 clk, low  3.4 clk
+
+    "	jc	007$\n"         //                                  2 [1/3]
+    "	ljmp	008$\n"     //                                  3 [3]
+
+    // bit "1" transmission
+    "007$:\n"
+    "	setb	_P5_5\n"    //                                  2 [1]                       1
+    "	nop	\n"             //                                                              2
+    "	nop	\n"             //                                                              3
+    "	clr	_P5_5\n"        //                                  2 [1]                       _1
+    "	ljmp	009$\n"     //                                  3 [3]                       _2 + 2
+
+    // bit "0" transmission
+    "008$:\n"
+    "	setb	_P5_5\n"    //                                  2 [1]                               1
+    // "	nop	\n"             //                                                                      2
+    "	clr	_P5_5\n"        //                                  2 [1]                               _1
+    "	nop	\n"             //                                                                      _2
+    "	nop	\n"             //                                                                      _3
+    "009$:\n"
+#else
+    COMPILE_TIME_ASSERT(false); // frequency not supported
+#endif
 
     // Here the single bit is over, so no precise timing required anymore.
     "	ljmp	004$\n"     //                                  3 [3]
