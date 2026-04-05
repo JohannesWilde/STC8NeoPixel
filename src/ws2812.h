36f874de8d6ea10b00b85d87bcfd845da57b8698
Johannes Wilde <git@jowil.de>
2026-04-05 19:17:26 +0200

    Even though this increases the binary size from 524 to 618 [delta = 94] this did work better in another project.
    The complete assembly implementation somehow executed this _show twice - I don't know why.

--- src/ws2812.h
+++ src/ws2812.h
@@ -4,6 +4,6 @@
 #include <stdint.h>
 
 
-void show(uint8_t const * data, uint8_t const length, uint8_t const brightness) __reentrant __naked;
+void show(uint8_t const * data, uint8_t const length, uint8_t const brightness);
 
 #endif // WS2812_H
